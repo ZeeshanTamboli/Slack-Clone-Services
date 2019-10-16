3d600619b108f3e86aff81ec8b387ccf9c6785da
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2019-10-16 22:24:02 +0530

    Added custom JSON responses

--- api/models/User.go
+++ api/models/User.go
@@ -1,24 +1,54 @@
 package models
 
 import (
+	"errors"
 	"html"
 	"strings"
 	"time"
+
+	"github.com/jinzhu/gorm"
+
+	"github.com/badoux/checkmail"
 )
 
 // User : Fields of a user
 type User struct {
-	ID         uint32    `gorm:"primary_key;auto_increment;not null" json:"id"`
-	Email      string    `gorm:"size:100;not null;unique" json:"email"`
-	Workspaces []string  `gorm:"size:100;not null" json:"workspaces"`
-	CreatedAt  time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt  time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	ID        uint32    `gorm:"primary_key;auto_increment;not null" json:"id"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email"`
+	Workspace string    `gorm:"size:100;not null" json:"workspace"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
 
 // Prepare : Prepares a new User object
 func (u *User) Prepare() {
 	u.ID = 0
 	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
+	u.Workspace = html.EscapeString(strings.TrimSpace(u.Workspace))
 	u.CreatedAt = time.Now()
 	u.UpdatedAt = time.Now()
+}
+
+// Validate : Validates the given input data
+func (u *User) Validate() error {
+	if u.Email == "" {
+		return errors.New("Email is required")
+	}
+
+	if u.Workspace == "" {
+		return errors.New("Workspace is required")
+	}
+
+	if err := checkmail.ValidateFormat(u.Email); err != nil {
+		return errors.New("Invalid Email")
+	}
+	return nil
+}
+
+// SaveUser : Save the user in the database
+func (u *User) SaveUser(db *gorm.DB) (*User, error) {
+	if err := db.Debug().Create(&u).Error; err != nil {
+		return &User{}, err
+	}
+	return u, nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/ZeeshanTamboli/slack-clone-services
 go 1.12
 
 require (
-	github.com/badoux/checkmail v0.0.0-20181210160741-9661bd69e9ad // indirect
+	github.com/badoux/checkmail v0.0.0-20181210160741-9661bd69e9ad
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/jinzhu/gorm v1.9.11
