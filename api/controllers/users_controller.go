ad4b317a2536208fc6a5818c475b77f61be51afc
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2019-10-19 19:25:18 +0530

    Wrote my first REST api in golang

--- api/controllers/users_controller.go
+++ api/controllers/users_controller.go
@@ -1,11 +1,33 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+
+	"github.com/ZeeshanTamboli/slack-clone-services/api/responses"
+
+	"github.com/ZeeshanTamboli/slack-clone-services/api/models"
 )
 
 // CreateWorkspace : Create user workspace
 func (server *Server) CreateWorkspace(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Body)
+	var err error
+	user := models.User{}
+
+	err = json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user.Prepare()
+	err = user.Validate()
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusCreated, user)
 }
