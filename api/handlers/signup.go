af0221c329a9ee5a2cb23bd1a2dd226779d72a41
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2019-12-01 17:39:52 +0530

    Many-to-many relationship in signup flow between users and workspaces finally done

--- api/handlers/signup.go
+++ api/handlers/signup.go
@@ -18,18 +18,38 @@ type Request struct {
 
 func createUserAndWorkspaceHandler(w http.ResponseWriter, r *http.Request) {
 	request := Request{}
+	var numOfRowsInserted int64
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
 		panic(err)
 	}
 
-	res, err := database.DBCon.Exec(`insert into users (first_name, last_name, email) values ($1, $2, $3)`, request.FirstName, request.LastName, request.Email)
+	var userID int
+	err = database.DBCon.QueryRow(`select id from users where email=$1`, request.Email).Scan(&userID)
+	if userID == 0 {
+		err = database.DBCon.QueryRow(`insert into users (first_name, last_name, email) values ($1, $2, $3) returning id`, request.FirstName, request.LastName, request.Email).Scan(&userID)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	var workspaceID int
+	err = database.DBCon.QueryRow(`select id from workspaces where name=$1`, request.Workspace).Scan(&workspaceID)
+	if workspaceID == 0 {
+		err = database.DBCon.QueryRow(`insert into workspaces (name) values ($1) returning id`, request.Workspace).Scan(&workspaceID)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	usersAndworkspacesRes, err := database.DBCon.Exec(`insert into users_workspaces (user_id, workspace_id) values ($1, $2)`, userID, workspaceID)
+	if err != nil {
+		panic(err)
+
+	}
+	numOfRowsInserted, err = usersAndworkspacesRes.RowsAffected()
 	if err != nil {
 		panic(err)
 	}
-	numOfRowsInserted, err := res.RowsAffected()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Number of rows inserted : %v", numOfRowsInserted)
+	fmt.Printf("Number of rows inserted in 'users_workspaces' table : %v \n", numOfRowsInserted)
 }
--- postgresql/signup.sql
+++ postgresql/signup.sql
@@ -1,7 +1,6 @@
 /* Creating users table */
 create table users(id serial primary key, first_name varchar(100), last_name varchar(100), email varchar(100) unique, created_at timestamptz default now(), updated_at timestamptz default now());
 
-
 /* Update timestamp when row is updated in PostgreSQL */
 CREATE OR REPLACE FUNCTION update_update_at_column()
 RETURNS TRIGGER AS $$
@@ -14,3 +13,26 @@ $$ language 'plpgsql';
 CREATE TRIGGER update_users_updated_at BEFORE UPDATE
 ON users FOR EACH ROW EXECUTE PROCEDURE 
 update_update_at_column();
+
+
+
+/* Creating Workspaces table */
+create table workspaces(id serial primary key, name varchar(100) unique, created_at timestamptz default now(), updated_at timestamptz default now());
+
+/* Update timestamp when row is updated in PostgreSQL */
+CREATE OR REPLACE FUNCTION update_update_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = now(); 
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_workspaces_updated_at BEFORE UPDATE
+ON workspaces FOR EACH ROW EXECUTE PROCEDURE 
+update_update_at_column();
+
+
+
+/* creating our bridge table */
+create table users_workspaces (user_id int, workspace_id int, foreign key(user_id) references users(id), foreign key(workspace_id) references workspaces(id), primary key(user_id, workspace_id));
