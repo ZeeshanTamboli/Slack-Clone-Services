0f80522a7f9b66e6b7c9ded8f1562da9a76add3b
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2020-01-07 22:30:00 +0530

    Added error validation handling

--- api/handlers/signup.go
+++ api/handlers/signup.go
@@ -2,8 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+
+	"github.com/ZeeshanTamboli/slack-clone-services/api/responses"
 
 	"github.com/ZeeshanTamboli/slack-clone-services/database"
 )
@@ -28,7 +31,8 @@ func createUserAndWorkspaceHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		panic(err)
+		responses.ERROR(w, http.StatusUnprocessableEntity, errors.New("Cannot decode request JSON"))
+		return
 	}
 
 	var userID int
@@ -36,7 +40,8 @@ func createUserAndWorkspaceHandler(w http.ResponseWriter, r *http.Request) {
 	if userID == 0 {
 		err = database.DBCon.QueryRow(`insert into users (first_name, last_name, email) values ($1, $2, $3) returning id`, request.FirstName, request.LastName, request.Email).Scan(&userID)
 		if err != nil {
-			panic(err)
+			responses.ERROR(w, http.StatusConflict, errors.New("Cannot insert user data"))
+			return
 		}
 	}
 
@@ -45,27 +50,23 @@ func createUserAndWorkspaceHandler(w http.ResponseWriter, r *http.Request) {
 	if workspaceID == 0 {
 		err = database.DBCon.QueryRow(`insert into workspaces (name) values ($1) returning id`, request.Workspace).Scan(&workspaceID)
 		if err != nil {
-			panic(err)
+			responses.ERROR(w, http.StatusConflict, errors.New("Cannot insert workspace data"))
+			return
 		}
 	}
 
 	usersAndworkspacesRes, err := database.DBCon.Exec(`insert into users_workspaces (user_id, workspace_id) values ($1, $2)`, userID, workspaceID)
 	if err != nil {
-		panic(err)
+		responses.ERROR(w, http.StatusConflict, errors.New("Cannot insert duplicate data into users_workspaces"))
+		return
 	}
 	numOfRowsInserted, err = usersAndworkspacesRes.RowsAffected()
 	if err != nil {
-		panic(err)
+		responses.ERROR(w, http.StatusConflict, errors.New("Rows affected throwed an error"))
+		return
 	}
 	fmt.Printf("Number of rows inserted in 'users_workspaces' table : %v \n", numOfRowsInserted)
 
 	response.Success = true
-	responseJSON, err := json.Marshal(response)
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(responseJSON)
+	responses.JSON(w, http.StatusOK, response)
 }
